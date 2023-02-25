bb5b750eb1315ee3c17467e973fb9fdc0f89aa94
Roman Stigler <fromanx@gmail.com>
2023-02-25 16:38:57 -0600

    added various changes to client and server

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,46 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                password
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($authors: String!, $description: String!, $bookId: String!, $image: String!, $link: String!) {
+        saveBook(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link) {
+            _id
+            title
+            authors
+            description
+        }
+    }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+        }
+    }
+`
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book} = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find().populate('savedBooks');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username }).populate('savedBooks');
+        },
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+          const user = await User.create({ username, email, password });
+          const token = signToken(user);
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No user found with this email address');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+    
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        saveBook: async () => {},
+        removeBook: async () => {},
+    },
+};
+
+module.exports = resolvers;
